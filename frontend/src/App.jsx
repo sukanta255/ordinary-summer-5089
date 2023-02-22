a71cdef54d441973f10f79cf8562724c612df116
Mohit Raj Singh <mohit94e@gmail.com>
2023-02-22 22:14:44 +0530

    day2 signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Box } from "@chakra-ui/react";
 import AllRoutes from "./components/Routes/AllRoutes";
+import SignUp from "./Pages/SignUp";
 
 function App() {
   return (
     <Box style={{fontFamily: 'Jost'}}>
-      <AllRoutes />
+      {/* <AllRoutes /> */}
+      <SignUp />
     </Box>
   );
 }
