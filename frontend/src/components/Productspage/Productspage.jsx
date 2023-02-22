354a7b7e29772fc776fffbc85b3c281697394c44
sayyedsharuk <sayyedsharuk75@gmail.com>
2023-02-22 10:53:10 +0530

    Bloatware Removed

---
+++ frontend/src/components/Productspage/Productspage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Flex, Heading } from "@chakra-ui/react";
+
+const Productspage = () => {
+  return (
+    <Flex>
+      <Heading>Hello To product Page</Heading>
+    </Flex>
+  );
+};
+
+export default Productspage;
