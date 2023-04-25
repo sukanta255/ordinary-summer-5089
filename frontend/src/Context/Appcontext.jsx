b2c6e38fe7da3f65ef663afc6334d06144ede3b4
sayyedsharuk <sayyedsharuk75@gmail.com>
2023-04-25 17:42:08 +0530

    Fixed Products Page Quantity

--- frontend/src/Context/Appcontext.jsx
+++ frontend/src/Context/Appcontext.jsx
@@ -2,168 +2,149 @@ import { useToast } from "@chakra-ui/react";
 import React, { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 export const Appcontext = createContext();
 
 const AppcontextProvider = ({ children }) => {
-    const [isAuth, setIsAuth] = useState(
-        localStorage.getItem("authToken") || false
-    );
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("authToken") || false);
 
-    const navigate = useNavigate();
-    const toast = useToast();
-    const token = localStorage.getItem("authToken");
+  const navigate = useNavigate();
+  const toast = useToast();
+  const token = localStorage.getItem("authToken");
 
+  const handleLogin = (e, user) => {
+    e.preventDefault();
 
-    const handleLogin = (e, user) => {
-        e.preventDefault();
-
-        let { email, password } = user;
-        if (email.includes("@kapdelo.com")) {
-            if (email && password) {
-                fetch("https://enchanting-gold-tie.cyclic.app/users/login", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(user),
-                })
-                    .then((res) => res.json())
-                    .then((data) => {
-                        // console.log(data);
-                        if (data.msg === "Login success") {
-                            localStorage.setItem("authToken", data.token)
-                            toast({
-                                description: "Admin Login Success",
-                                status: 'success',
-                                duration: 2000,
-                                isClosable: true,
-                            })
-                            navigate("/admin", { replace: true });
-                        }
-                        else if (data.msg === "Invaild credentials") {
-                            toast({
-                                description: 'Invaild credentials',
-                                status: 'error',
-                                duration: 1000,
-                                isClosable: true,
-                            })
-                            navigate("/login", { replace: true });
-                        }
-                    })
-                    .catch((err) => console.log(err));
-                // toast({
-                //     description: 'Invaild credentials',
-                //     status: 'error',
-                //     duration: 1000,
-                //     isClosable: true,
-                // })
-
+    let { email, password } = user;
+    if (email.includes("@kapdelo.com")) {
+      if (email && password) {
+        fetch("https://enchanting-gold-tie.cyclic.app/users/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            // console.log(data);
+            if (data.msg === "Login success") {
+              localStorage.setItem("authToken", data.token);
+              toast({
+                description: "Admin Login Successfully",
+                status: "success",
+                duration: 2000,
+                isClosable: true,
+              });
+              navigate("/admin", { replace: true });
+            } else if (data.msg === "Invaild credentials") {
+              toast({
+                description: "Invaild credentials",
+                status: "error",
+                duration: 1000,
+                isClosable: true,
+              });
+              navigate("/login", { replace: true });
             }
-            else {
-                alert("Please Sign Up first");
+          })
+          .catch((err) => console.log(err));
+        // toast({
+        //     description: 'Invaild credentials',
+        //     status: 'error',
+        //     duration: 1000,
+        //     isClosable: true,
+        // })
+      } else {
+        alert("Please Sign Up first");
+      }
+    } else {
+      if (email && password) {
+        fetch("https://enchanting-gold-tie.cyclic.app/users/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            // console.log(data);
+            if (data.msg === "Login success") {
+              localStorage.setItem("authToken", data.token);
+              toast({
+                description: "Login Successfully",
+                status: "success",
+                duration: 2000,
+                isClosable: true,
+              });
+              navigate("/", { replace: true });
+            } else if (data.msg === "Invaild credentials") {
+              toast({
+                title: "An error occurred.",
+                description: "Invaild credentials",
+                status: "error",
+                duration: 1000,
+                isClosable: true,
+              });
+              navigate("/login", { replace: true });
             }
-        }
-        else {
-
-            if (email && password) {
-                fetch("https://enchanting-gold-tie.cyclic.app/users/login", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(user),
-                })
-                    .then((res) => res.json())
-                    .then((data) => {
-                        // console.log(data);
-                        if (data.msg === "Login success") {
-                            localStorage.setItem("authToken", data.token)
-                            toast({
-                                description: "Login Success",
-                                status: 'success',
-                                duration: 2000,
-                                isClosable: true,
-                            })
-                            navigate("/", { replace: true });
-                        }
-                        else if (data.msg === "Invaild credentials") {
-                            toast({
-                                title: 'An error occurred.',
-                                description: 'Invaild credentials',
-                                status: 'error',
-                                duration: 1000,
-                                isClosable: true,
-                            })
-                            navigate("/login", { replace: true });
-                        }
-                    })
-                    .catch((err) => console.log(err));
-                toast({
-                    description: 'Invaild credentials',
-                    status: 'error',
-                    duration: 1000,
-                    isClosable: true,
-                })
-
-            }
-            else {
-                alert("Please Sign Up first");
-            }
-        }
+          })
+          .catch((err) => console.log(err));
+        toast({
+          description: "Invaild credentials",
+          status: "error",
+          duration: 1000,
+          isClosable: true,
+        });
+      } else {
+        alert("Please Sign Up first");
+      }
     }
+  };
 
-    const handleSignup = (e, user) => {
-        e.preventDefault();
+  const handleSignup = (e, user) => {
+    e.preventDefault();
 
-        let { firstname, lastname, email, password } = user;
-        if (firstname && lastname && email && password) {
-            fetch("https://enchanting-gold-tie.cyclic.app/users/register", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(user),
-            })
-                .then((res) => res.json())
-                .then((data) => {
-                    console.log(data);
-                    if (data.msg === "User registered.") {
-                        toast({
-                            description: "Registered Sucessfully",
-                            status: 'success',
-                            duration: 2000,
-                            isClosable: true,
-                        })
-                        navigate("/login");
-                    }
-                    else if (data.msg === "Please login, user already exist") {
-                        toast({
-                            description: "Please login, user already exist",
-                            status: 'error',
-                            duration: 2000,
-                            isClosable: true,
-                        })
-                        navigate("/login");
-                    }
-                })
-                .catch((err) => console.log(err));
-        }
-        else {
-            alert("Please provide all fields.");
-        }
-    };
+    let { firstname, lastname, email, password } = user;
+    if (firstname && lastname && email && password) {
+      fetch("https://enchanting-gold-tie.cyclic.app/users/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.msg === "User registered.") {
+            toast({
+              description: "Registered Sucessfully",
+              status: "success",
+              duration: 2000,
+              isClosable: true,
+            });
+            navigate("/login");
+          } else if (data.msg === "Please login, user already exist") {
+            toast({
+              description: "Please login, user already exist",
+              status: "error",
+              duration: 2000,
+              isClosable: true,
+            });
+            navigate("/login");
+          }
+        })
+        .catch((err) => console.log(err));
+    } else {
+      alert("Please provide all fields.");
+    }
+  };
 
-
-
-
-    const data = {
-        handleLogin,
-        handleSignup,
-    };
-    return <Appcontext.Provider value={data}>{children}</Appcontext.Provider>;
-
-
-}
+  const data = {
+    handleLogin,
+    handleSignup,
+  };
+  return <Appcontext.Provider value={data}>{children}</Appcontext.Provider>;
+};
 
 export default AppcontextProvider;
