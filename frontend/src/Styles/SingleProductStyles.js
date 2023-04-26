ef86236d96989bba904cb38f3bb41ebe138ff4cb
sayyedsharuk <sayyedsharuk75@gmail.com>
2023-04-26 10:30:21 +0530

    Updated Home Page API

---
+++ frontend/src/Styles/SingleProductStyles.js
@@ -0,0 +1,236 @@
+export const singleProd = {
+  loading_main: {
+    justifyContent: "center",
+    h: "600px",
+    w: "100%",
+  },
+  spinner: {
+    thickness: "4px",
+    speed: "0.65s",
+    emptyColor: "gray.200",
+    color: "black.500",
+    size: "xl",
+  },
+  error: {
+    direction: "column",
+    w: "100%",
+    h: "600px",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  errImg: {
+    w: "400px",
+    src: "https://user-images.githubusercontent.com/112304655/218245639-36aca8c4-66d4-4350-81f0-119fb68f7ca7.gif",
+  },
+  main: {
+    direction: "column",
+    w: "100%",
+  },
+  subMain: {
+    mx: "12",
+    mt: "8",
+    direction: { base: "column", sm: "column", md: "row" },
+  },
+};
+export const imgBox = {
+  main: {
+    flex: 1,
+  },
+  show: {
+    above: "lg",
+  },
+  wrapper: {
+    flex: 1,
+    direction: "column",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  img: {
+    p: "3",
+    alt: "Broken Data",
+    w: "100%",
+  },
+  icoMain: {
+    flex: 4,
+  },
+  icoSubMain: {
+    w: "100%",
+    position: "relative",
+  },
+  brokeImg: {
+    p: "3",
+    alt: "Broken Data",
+    w: "100%",
+  },
+  hide: {
+    above: "md",
+  },
+  iconBtn: {
+    position: "absolute",
+    top: "30px",
+    left: "30px",
+    transition: "550ms",
+    borderRadius: "50%",
+    bgColor: "white",
+    _hover: { color: "white", bgColor: "black" },
+  },
+};
+export const descBox = {
+  main: {
+    flex: 1,
+    my: "8",
+    direction: "column",
+    justifyContent: "flex-start",
+  },
+  titleMain: {
+    justifyContent: "space-between",
+  },
+  text: {
+    as: "b",
+    textTransform: "capitalize",
+    letterSpacing: "2px",
+    noOfLines: "2",
+    fontSize: { base: "xl", sm: "2xl" },
+  },
+  show: {
+    above: "md",
+  },
+  icon: {
+    transition: "550ms",
+    borderRadius: "50%",
+    bgColor: "white",
+    _hover: { color: "white", bgColor: "black" },
+  },
+};
+export const price = {
+  main: {
+    gap: 3,
+    my: "3",
+  },
+  price: {
+    as: "b",
+    textTransform: "capitalize",
+    letterSpacing: "2px",
+    noOfLines: "2",
+    fontSize: "lg",
+  },
+  priceReduced: {
+    as: "s",
+    textTransform: "capitalize",
+    color: "grey",
+    letterSpacing: "2px",
+    noOfLines: "2",
+    fontSize: "lg",
+  },
+};
+export const views = {
+  main: {
+    my: 3,
+    gap: "4",
+    alignItems: "center",
+  },
+  text: {
+    color: "gray",
+    textTransform: "capitalize",
+    noOfLines: "2",
+    fontSize: "sm",
+  },
+};
+export const cloth = {
+  main: {
+    my: "3",
+    direction: "column",
+  },
+  text: {
+    as: "b",
+    mb: "2",
+  },
+};
+export const color = {
+  main: {
+    my: "3",
+    direction: "column",
+  },
+  text: {
+    as: "b",
+    mb: "2",
+  },
+  color: {
+    mt: "1",
+    w: "10",
+    h: "10",
+    border: "4px solid white",
+    boxShadow: "outline",
+    borderRadius: "50%",
+  },
+};
+export const qty = {
+  main: {
+    direction: "column",
+    alignItems: "flex-start",
+  },
+  text: {
+    as: "b",
+    mb: "2",
+  },
+  sub: {
+    justifyContent: "flex-start",
+  },
+  incBtn: {
+    transition: "550ms",
+    bgColor: "white",
+    _hover: { color: "white", bgColor: "black" },
+  },
+  decBtn: {
+    transition: "550ms",
+    bgColor: "white",
+    _hover: { color: "white", bgColor: "black" },
+  },
+};
+
+export const total = {
+  main: {
+    direction: "column",
+    alignItems: "flex-start",
+  },
+  head: {
+    as: "b",
+    mb: "2",
+  },
+  text: {
+    as: "b",
+    borderRadius: "6px",
+    border: "2px solid black",
+    p: 2,
+  },
+  submitBtn: {
+    mt: "8",
+    type: "submit",
+    value: "Add to Cart",
+    borderColor: "black",
+    bgColor: "black",
+    color: "white",
+    _hover: { bgColor: "white", color: "black" },
+  },
+};
+export const tabs = {
+  main: {
+    mx: "12",
+    mt: "8",
+    direction: "column",
+  },
+  panel: {
+    w: "100%",
+    direction: "column",
+    alignItems: "flex-start",
+  },
+  panel2: {
+    direction: "column",
+    alignItems: "center",
+  },
+  panel3: {
+    w: "100%",
+    direction: "column",
+    alignItems: "flex-start",
+  },
+};
--- frontend/src/components/Productspage/SingleProductPage.jsx
+++ frontend/src/components/Productspage/SingleProductPage.jsx
@@ -28,6 +28,7 @@ import { useParams } from "react-router-dom";
 import SizeBar from "./SizeBar";
 import { useForm } from "react-hook-form";
 import { getFullData } from "../../API/ProductRequests";
+import { cloth, color, descBox, imgBox, price, qty, singleProd, tabs, total, views } from "../../Styles/SingleProductStyles";
 
 const SingleProductPage = () => {
   const toast = useToast();
@@ -103,78 +104,57 @@ const SingleProductPage = () => {
   const group = getRootProps();
 
   return loading ? (
-    <Flex justifyContent={"center"} h={"600px"} w={"100%"}>
-      <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="black.500" size="xl" />
+    <Flex {...singleProd.loading_main}>
+      <Spinner {...singleProd.spinner} />
     </Flex>
   ) : error ? (
-    <Flex direction={"column"} w={"100%"} h={"600px"} justifyContent={"center"} alignItems={"center"}>
-      <Image w={"400px"} src="https://user-images.githubusercontent.com/112304655/218245639-36aca8c4-66d4-4350-81f0-119fb68f7ca7.gif" />
+    <Flex {...singleProd.error}>
+      <Image {...singleProd.errImg} />
     </Flex>
   ) : (
     <form onSubmit={onSubmit}>
-      <Flex direction={"column"} w={"100%"}>
+      <Flex {...singleProd.main}>
         {/* Main Box */}
-        <Flex mx={"12"} mt={"8"} direction={{ base: "column", sm: "column", md: "row" }}>
+        <Flex {...singleProd.subMain}>
           {/* Image Box */}
-          <Flex flex={1}>
-            <Show above={"lg"}>
-              <Flex flex={1} direction={"column"} justifyContent={"flex-start"} alignItems={"center"}>
-                <Image p={"3"} src={data.image} alt={"Broken Data"} w={"100%"} />
+          <Flex {...imgBox.main}>
+            <Show {...imgBox.show}>
+              <Flex {...imgBox.wrapper}>
+                <Image src={data.image} {...imgBox.img} />
               </Flex>
               <Divider orientation="vertical" />
             </Show>
-            <Flex flex={4}>
-              <Flex w={"100%"} position={"relative"}>
-                <Image p={"3"} src={data.image} alt={"Broken Data"} w={"100%"} />
-                <Hide above="md">
-                  <IconButton
-                    position={"absolute"}
-                    top={"30px"}
-                    left={"30px"}
-                    transition={"550ms"}
-                    icon={<BsStar />}
-                    borderRadius={"50%"}
-                    bgColor={"white"}
-                    _hover={{ color: "white", bgColor: "black" }}
-                  />
+            <Flex {...imgBox.icoMain}>
+              <Flex {...imgBox.icoSubMain}>
+                <Image src={data.image} {...imgBox.brokeImg} />
+                <Hide {...imgBox.hide}>
+                  <IconButton icon={<BsStar />} {...imgBox.iconBtn} />
                 </Hide>
               </Flex>
             </Flex>
           </Flex>
           {/* Description Box */}
-          <Flex flex={1} my={"8"} direction={"column"} justifyContent={"flex-start"}>
+          <Flex {...descBox.main}>
             {/* Title and wishlist button */}
-            <Flex justifyContent={"space-between"}>
-              <Text as={"b"} textTransform={"capitalize"} letterSpacing={"2px"} noOfLines={"2"} fontSize={{ base: "xl", sm: "2xl" }}>
-                {data.description}
-              </Text>
-              <Show above="md">
-                <IconButton transition={"550ms"} icon={<BsStar />} borderRadius={"50%"} bgColor={"white"} _hover={{ color: "white", bgColor: "black" }} />
+            <Flex {...descBox.titleMain}>
+              <Text {...descBox.text}>{data.description}</Text>
+              <Show {...descBox.show}>
+                <IconButton icon={<BsStar />} {...descBox.icon} />
               </Show>
             </Flex>
             {/* price Section */}
-            <Flex gap={3} my={"3"}>
-              <Text as={"b"} textTransform={"capitalize"} letterSpacing={"2px"} noOfLines={"2"} fontSize={"lg"}>
-                $ {data.price}
-              </Text>
-              {data.reducedPrice ? (
-                <Text as={"s"} textTransform={"capitalize"} color={"grey"} letterSpacing={"2px"} noOfLines={"2"} fontSize={"lg"}>
-                  $ {data.reducedPrice}
-                </Text>
-              ) : null}
+            <Flex {...price.main}>
+              <Text {...price.price}>$ {data.price}</Text>
+              {data.reducedPrice ? <Text {...price.priceReduced}>$ {data.reducedPrice}</Text> : null}
             </Flex>
             {/* Dummy View Users  */}
-            <Flex my={3} gap={"4"} alignItems={"center"}>
+            <Flex {...views.main}>
               <BsFillEyeFill style={{ color: "gray" }} />
-              <Text color={"gray"} textTransform={"capitalize"} noOfLines={"2"} fontSize={"sm"}>
-                {randomViewer} people are viewing this right now
-              </Text>
+              <Text {...views.text}>{randomViewer} people are viewing this right now</Text>
             </Flex>
             {/* Cloth Sizes */}
-            <Flex my={"3"} direction={"column"}>
-              <Text as={"b"} mb={"2"}>
-                Size: S
-              </Text>
+            <Flex {...cloth.main}>
+              <Text {...cloth.text}>Size: S</Text>
               <HStack {...group}>
                 {options.map((value) => {
                   const radio = getRadioProps({ value });
@@ -189,55 +169,36 @@ const SingleProductPage = () => {
 
             {/* Color Section */}
             {data?.colour ? (
-              <Flex my={"3"} direction={"column"}>
-                <Text as={"b"} mb={"2"}>
-                  Color: {data?.colour}
-                </Text>
-                <Flex mt={"1"} w={"10"} h={"10"} border={"4px solid white"} boxShadow={"outline"} borderRadius={"50%"} bgColor={data?.colour}></Flex>
+              <Flex {...color.main}>
+                <Text {...color.text}>Color: {data?.colour}</Text>
+                <Flex {...color.color} bgColor={data?.colour}></Flex>
               </Flex>
             ) : null}
 
             {/* Quantity Section */}
-            <Flex direction={"column"} alignItems={"flex-start"}>
-              <Text as={"b"} mb={"2"}>
-                Quantity
-              </Text>
-              <Flex justifyContent={"flex-start"}>
-                <IconButton
-                  isDisabled={quantity === "6"}
-                  transition={"550ms"}
-                  icon={<BsPlus style={{ fontSize: "24px" }} />}
-                  bgColor={"white"}
-                  _hover={{ color: "white", bgColor: "black" }}
-                  onClick={() => handleQuantity(+1)}
-                />
+            <Flex {...qty.main}>
+              <Text {...qty.text}>Quantity</Text>
+              <Flex {...qty.sub}>
+                <IconButton {...qty.incBtn} isDisabled={quantity === "6"} icon={<BsPlus style={{ fontSize: "24px" }} />} onClick={() => handleQuantity(+1)} />
                 <FormControl>
                   <PinInput value={quantity} onChange={(e) => setQuantity(e.target.value)}>
                     <PinInputField {...register("firstName", { min: 1, max: 15 })} />
                   </PinInput>
                 </FormControl>
-                <IconButton
-                  isDisabled={quantity === "1"}
-                  transition={"550ms"}
-                  icon={<AiOutlineMinus />}
-                  bgColor={"white"}
-                  _hover={{ color: "white", bgColor: "black" }}
-                  onClick={() => handleQuantity(-1)}
-                />
+                <IconButton {...qty.decBtn} isDisabled={quantity === "1"} icon={<AiOutlineMinus />} onClick={() => handleQuantity(-1)} />
               </Flex>
             </Flex>
             {/* Total Amount Based on Quantity */}
-            <Flex direction={"column"} alignItems={"flex-start"}>
-              <Text as={"b"} mb={"2"}>
-                Total Price
-              </Text>
-              <Text as={"b"} borderRadius={"6px"} border={"2px solid black"} p={2}>{data.price * quantity}$</Text>
+            <Flex {...total.main}>
+              <Text {...total.head}>Total Price</Text>
+              <Text {...total.text}>{data.price * quantity}$</Text>
             </Flex>
             {/* Add to Cart Btn */}
-            <Input mt={"8"} type={"submit"} value={"Add to Cart"} borderColor={"black"} bgColor={"black"} color={"white"} _hover={{ bgColor: "white", color: "black" }} />
+            <Input {...total.submitBtn} />
           </Flex>
         </Flex>
-        <Flex mx={"12"} mt={"8"} direction={"column"}>
+        {/** Tabs View */}
+        <Flex {...tabs.main}>
           <Tabs align="center">
             <TabList>
               <Tab>Product Description</Tab>
@@ -247,12 +208,12 @@ const SingleProductPage = () => {
 
             <TabPanels>
               <TabPanel>
-                <Flex w={"100%"} direction={"column"} alignItems={"flex-start"}>
+                <Flex {...tabs.panel}>
                   <Text>{data.productDescription}</Text>
                 </Flex>
               </TabPanel>
               <TabPanel>
-                <Flex direction={"column"} alignItems={"center"}>
+                <Flex {...tabs.panel2}>
                   <Text fontSize={"sm"}>India-wide Shipping Average time: 4-6 days after the order confirmation.</Text>
                   <Text as={"b"}>SHIPPING POLICY</Text>
                   <Text fontSize={"sm"}>
@@ -272,7 +233,7 @@ const SingleProductPage = () => {
                 </Flex>
               </TabPanel>
               <TabPanel>
-                <Flex w={"100%"} direction={"column"} alignItems={"flex-start"}>
+                <Flex {...tabs.panel3}>
                   <Text>Wash inside out</Text>
                   <Text>Wash with similar colours</Text>
                 </Flex>
