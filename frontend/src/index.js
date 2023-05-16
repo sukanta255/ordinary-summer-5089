5d599102faa9219df58aea8d28f657d296de50a4
Abhisek753 <properachievement@gmail.com>
2023-05-16 22:01:28 +0530

    icon add

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AuthProvider } from "./Context/AuthContext";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
